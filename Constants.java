ea6fe9b021be81fa5aa49d340e8179fe6ec59136
Zach <thezachrose@gmail.com>
2018-04-01 09:06:35 -0500

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,11 @@
+public interface Constants
+{
+    public static final char LEFT_PAR = '(';
+    public static final char RIGHT_PAR = ')';
+    public static final char LEFT_CURLY = '{';
+    public static final char RIGHT_CURLY = '}';
+    public static final char LEFT_BRACKET = '[';
+    public static final char RIGHT_BRACKET = ']';
+    public static final char SLASH = '/';
+    public static final char STAR = '*';
+}
